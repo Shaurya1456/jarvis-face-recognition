563cd3b3b164731740fdd7d4abe34f9d3e93cfd7
Quentin Comte-Gaz <quentin@comte-gaz.com>
2017-05-13 18:15:16 +0200

    plugin: Add base configuration

---
+++ config.sh
@@ -0,0 +1,9 @@
+# Configure the camera tu use
+#var_jv_pg_fr_use_pi_camera="True" # Use Pi camera: True / Use USB camera: False
+#var_jv_pg_fr_usb_camera_id=0 # USB camera number (0 by default)
+
+# List people to find in pictures
+var_jv_pg_fr_people_to_find='[
+    {"name": "Barack Obama", "image": "/home/pi/images/barack_obama.jpg"},
+    {"name": "Hillary Clinton", "image": "/home/pi/images/hillary.jpg"}
+]'
