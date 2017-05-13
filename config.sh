3a20c9929481b5d46438df18be6261a8f8c11cd5
Quentin Comte-Gaz <quentin@comte-gaz.com>
2017-05-13 18:18:12 +0200

    config: Add a default person

--- config.sh
+++ config.sh
@@ -3,6 +3,8 @@
 #var_jv_pg_fr_usb_camera_id=0 # USB camera number (0 by default)
 
 # List people to find in pictures
+jv_pg_fr_default_person="Unknown"
+
 var_jv_pg_fr_people_to_find='[
     {"name": "Barack Obama", "image": "/home/pi/images/barack_obama.jpg"},
     {"name": "Hillary Clinton", "image": "/home/pi/images/hillary.jpg"}
