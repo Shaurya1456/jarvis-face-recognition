56b750890d7a3ca21e86b7b11ef7f5879db926a3
Quentin Comte-Gaz <quentin@comte-gaz.com>
2017-05-14 14:53:21 +0200

    plugin: For debug purpose, comment non verbose commands

--- functions.sh
+++ functions.sh
@@ -8,9 +8,9 @@ jv_pg_fr_start()
   # Start the server
   local dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-  if [ "$verbose" = true ]; then
+  #if [ "$verbose" = true ]; then
     python3 $dir/script/face_recognition_server.py --verbose="True" --knownPeople="$var_jv_pg_fr_people_to_find" --unknownPerson "$jv_pg_fr_unknown_person" &
-  else
-    nohup python3 $dir/script/face_recognition_server.py --verbose="True" --knownPeople="$var_jv_pg_fr_people_to_find" --unknownPerson "$jv_pg_fr_unknown_person" 2>&1 | jv_add_timestamps >>$jv_dir/jarvis.log &
-  fi
+  #else
+  #  nohup python3 $dir/script/face_recognition_server.py --verbose="True" --knownPeople="$var_jv_pg_fr_people_to_find" --unknownPerson "$jv_pg_fr_unknown_person" 2>&1 | jv_add_timestamps >>$jv_dir/jarvis.log &
+  #fi
 }
